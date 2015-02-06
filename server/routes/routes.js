65e0f4d2d9b31c2a840a2fd01b6c743784f3d7b5
Ian Pfeffer <ian.pfeffer@bbiengineering.com>
2015-02-05 23:57:47 -0700

    mmmm

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,7 +6,7 @@
 module.exports = function(app) {
 
     app.get('/ian', function(req, res) {
-        // load the single view file (angular will handle the page changes on the front-end)
+        // load the singlesd view file (angular will handle the page changes on the front-end)
         res.sendfile('./public/app/html/index.html');
     });
 };
