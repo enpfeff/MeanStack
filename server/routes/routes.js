e1df3e418d2af80a936c4b53aab985a5eb4b8e75
Ian Pfeffer <ian.pfeffer@bbiengineering.com>
2015-03-11 20:35:42 -0600

    Moving server side pages to be grouped

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,6 +7,6 @@ module.exports = function(app) {
 
     app.get('/FoodElement', function(req, res) {
         // load the singlesd view file (angular will handle the page changes on the front-end)
-        res.sendfile('./public/app/html/index.html');
+        res.sendfile('./server/pages/index.html');
     });
 };
